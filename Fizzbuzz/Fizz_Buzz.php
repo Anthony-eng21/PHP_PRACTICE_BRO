cb7f9c618d63c5dd6752b0e935b42b9ba1af6096
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-07-20 22:32:37 -0700

    buzz buzz buzz

---
+++ Fizzbuzz/Fizz_Buzz.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i = 0; $i < 100; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0)
+        echo "Fizz-Buzz";
+    else if ($i % 3 == 0)
+        echo "Fizz";
+    else if ($i % 5 == 0)
+        echo "buzz";
+    else
+        echo $i;
+    echo "<br>"; //dont forget to add this lol
+}
