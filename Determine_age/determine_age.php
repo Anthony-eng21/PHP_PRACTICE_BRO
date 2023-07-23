c0c19ca559a171ec0fe17d1ed1e45823ca806919
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-07-22 18:55:29 -0700

    comparing ages in objects

--- Determine_age/determine_age.php
+++ Determine_age/determine_age.php
@@ -25,9 +25,24 @@ class Person
 	{
 		// Challenge: define this function
 		// strtotime() Parse about any English textual datetime description into a Unix timestamp
-		// right now in years minus the Y that the use put in the MM-DD-YY format
+		// The year right now minus the Y that the use put in the MM-DD-YY format
+
 		return $year = (date('Y') - date('Y', strtotime($this->dob)));
 	}
+}
+
+// We can access these methods here becasue objects $p1 and $p2 are instances of the Person 
+// class, and as such, they have access to the public methods defined in that class. get_age() name etc
+function compare_ages($p1, $p2)
+{
+	// Challenge: define this function
+	if ($p1->get_age() > $p2->get_age()) {
+		return $p1->get_name() . ' is older than ' . $p2->get_name();
+	} else if ($p1->get_age() > $p2->get_age()) {
+		return $p1->get_name() . ' is younger than ' . $p2->get_name();
+	}
+	
+	return $p1->get_name() . ' and ' . $p2->get_name() . ' are the same age.';
 }
 
 
@@ -37,7 +52,10 @@ $erin = new Person('Erin', '1991-08-28');
 $teresa = new Person('Teresa', '2017-03-06');
 $lou = new Person('Lou', '2020-07-12');
 
+
 echo $joe->get_name() . ' is ' . $joe->get_age() . '<br/>';
 echo $erin->get_name() . ' is ' . $erin->get_age() . '<br/>';
 echo $teresa->get_name() . ' is ' . $teresa->get_age() . '<br/>';
-echo $lou->get_name() . ' is ' . $lou->get_age();
+echo $lou->get_name() . ' is ' . $lou->get_age() . '<br/>'; 
+
+echo compare_ages($joe, $erin);
